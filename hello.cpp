937bdb6bc7c32a96ae24b09cc130be6de2730940
iamaditya <adity1s2h@gmail.com>
2020-10-29 17:59:34 +0530

    integer added

--- hello.cpp
+++ hello.cpp
@@ -3,5 +3,8 @@ using namespace std;
 
 int main(){
     cout<<"Hello! Aditya is learning git";
+    cout<<"\ni am learning git status command ";
+    int n=3;
+    cout<<"\nthis no is "<<n<<"";
     return 0;
 }
